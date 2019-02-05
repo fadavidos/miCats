a17bc0273e825275d0adb066ae497d173ef86786
fabianosorio <fabianosorio@seven4n.com>
2019-02-05 13:05:59 -0500

    Se inician test sobre Applicative

--- Functor/src/test/scala/com/miCats/Functor/ApplicativeTest.scala
+++ Functor/src/test/scala/com/miCats/Functor/ApplicativeTest.scala
@@ -7,7 +7,7 @@ class ApplicativeTest extends FunSuite {
   /*
     Applicative Functors o más fácil Applicative: Applicative[F[_]]
     * Extiende de Functor
-    * Tiene una función `pure`, que permite embolver un dato dentro de un Functor =>  def pure[A](x: A): F[A]
+    * Tiene una función `pure`, que permite embolver un dato dentro de un type constructor =>  def pure[A](x: A): F[A]
 
    */
 
@@ -22,5 +22,86 @@ class ApplicativeTest extends FunSuite {
       valorOpcional
     }
   }
+
+  test("Cartesian Builder") {
+    import cats.data.Validated
+    import cats.data.Validated.{Valid, Invalid}
+    import cats.syntax.apply._
+
+    def validarMayoriaEdad(edad: Int): Validated[String, Int] = {
+      if ( edad >= 18 )
+        Valid(edad)
+      else
+        Invalid("Aún no es mayor de edad")
+    }
+
+    def validarEmail(email: String): Validated[String, String] = {
+      if( email.contains("@"))
+        Valid(email)
+      else
+        Invalid("El correo no es correcto")
+    }
+
+    def validarGenero(char: Char): Validated[String, Char] = {
+      if(char.equals('M') || char.equals('F'))
+        Valid(char)
+      else
+        Invalid("El genero no existe")
+    }
+
+    case class Usuario(nombre: String, edad: Int, email: String, genero: Char)
+
+    val miUsario =Usuario("Juan", 10, "invalido",'M')
+
+//    (
+//      validarEmail(miUsario.email),
+//      validarMayoriaEdad(miUsario.edad),
+//      validarGenero(miUsario.genero)
+//    ).mapN((mail, edad, genero) => mail )
+
+
+
+
+
+
+  }
+
+  test("por que no sirve") {
+    import cats.implicits._
+
+    val f: (Int, Char) => Double = (i, c) => (i + c).toDouble
+
+    val int: Option[Int] = Some(5)
+    val char: Option[Char] = Some('a')
+    val x: Option[Char => Double] = int.map(i => (c: Char) => f(i, c)) // what now?
+
+
+  }
+
+  test("traverse") {
+    import cats.syntax.traverse._
+    import cats.instances.list._
+    import cats.instances.option._
+
+    val list = List(0, 1,2,3,4)
+
+    def table2( number: Int ): Option[Int] = {
+      if(number == 0)
+        None
+      else
+        Some(number * 2)
+    }
+
+    val result: Option[List[Int]] = list.traverse[Option, Int](table2)
+
+    println(result)
+  }
+
+
+
+
+
+
+
 
 }
